beae6f55064639b857871a32c0a90cdd2af140cb
Preethi Kotaru <preethi.kotaruu@gmail.com>
2026-04-23 23:45:11 -0500

    Remove default weather forecast API

--- backend/SplitSmart.API/Program.cs
+++ backend/SplitSmart.API/Program.cs
@@ -22,26 +22,6 @@ if (app.Environment.IsDevelopment()) //Only enable Swagger in development (local
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapPost("/groups", (Group group) =>
 {
     group.Id = nextGroupId;
@@ -287,10 +267,6 @@ app.MapGet("/groups/{id}/members", (int id) =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 public class Group
 {
     public int Id { get; set; }
